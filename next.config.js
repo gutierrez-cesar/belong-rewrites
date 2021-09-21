16c7ebde9d5f5eb448d6a20037148d350124f047
Cesar Gutierrez <gutierrez.cesar@belonghome.com>
2021-09-21 12:12:44 -0700

    Changed origin from admin to staging

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
       fallback: [
         {
           source: "/:path*",
-          destination: `https://dualstack.awseb-awseb-6lr5azyfnmkz-244186263.us-west-1.elb.amazonaws.com/:path*`,
+          destination: `https://dualstack.awseb-awseb-1lx8eb9btctnt-1054739197.us-west-1.elb.amazonaws.com/:path*`,
         },
       ],
     }
