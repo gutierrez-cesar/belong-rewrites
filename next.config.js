ccf26bb2a3de548e660f0edd0984231617de7a49
Cesar Gutierrez <gutierrez.cesar@belonghome.com>
2021-09-21 12:10:20 -0700

    changed rewrite to https

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
       fallback: [
         {
           source: "/:path*",
-          destination: `http://dualstack.awseb-awseb-6lr5azyfnmkz-244186263.us-west-1.elb.amazonaws.com/:path*`,
+          destination: `https://dualstack.awseb-awseb-6lr5azyfnmkz-244186263.us-west-1.elb.amazonaws.com/:path*`,
         },
       ],
     }
